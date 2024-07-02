373c66a476b13d75c8ff5a7dc6274a070dc83da1
Joyal <joyaljoykarunagappally@gmail.com>
2024-07-02 22:32:35 +0530

    Added Lists

---
+++ 16Lists.py
@@ -0,0 +1,14 @@
+#List = used to multiple items in a single variable
+
+food = ["pizza", "cake", "Burger", "pudding"]
+
+# food[0] = "Bun"                 #Replaces with index value
+# food.append("Ice Cream")        #insert to last index of the list
+# food.remove("cake")             #Remove the value
+# food.pop()                      # Removes the last index value
+# food.insert(1, "Donut")         #Inserts elements using the index values
+# food.sort()                     #Sort the elements in the list
+# food.clear()                    #clear all elements in the list
+
+for i in food:
+    print(i)
