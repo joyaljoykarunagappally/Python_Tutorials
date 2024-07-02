cb092acab755db7bcc745df243842c2e9cf20eca
Joyal <joyaljoykarunagappally@gmail.com>
2024-07-02 22:57:42 +0530

    Added 2D Lists

---
+++ 17_2DLists.py
@@ -0,0 +1,10 @@
+# 2D Lists = a list of lists
+
+drinks = ["Rose Milk", "Bad ham Milk", "Strawberry Water"]
+dinner = ["pizza", "Burger", "Mutton"]
+dessert = ["Cake", "Ice Cream"]
+
+food = [drinks, dinner, dessert]
+
+# print(food[0])          # prints the index based list here first
+print(food[0][1])         # prints the index value of list of index list
