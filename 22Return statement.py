4efbceec09efd09047a2635222ec74401b7a67e9
Joyal <joyaljoykarunagappally@gmail.com>
2024-07-03 22:56:29 +0530

    Added return statement

---
+++ 22Return statement.py
@@ -0,0 +1,10 @@
+# return statement = Functions send python values/objects back to their caller.
+#                       These values are known as the function's return value.
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+result = multiply(4, 5)
+
+print(result)
