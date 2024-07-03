a2193775759b4adce5a4de1d332d5e19ec3a7c40
Joyal <joyaljoykarunagappally@gmail.com>
2024-07-03 22:44:13 +0530

    Added Functions

---
+++ 21Functions.py
@@ -0,0 +1,9 @@
+# Functions = A block of code executed only when it is called
+
+def hello(f_name, l_name, age):  # a function with 3 parameters
+    print("hello " + f_name + " " + l_name)
+    print("You are " + str(age) + " Years Old")
+
+
+hello("Joyal", "Joy", 21)       #function called with 3 arguments
+
