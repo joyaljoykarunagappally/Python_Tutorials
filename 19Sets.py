74affabc2f8a4a5b5eba6fe2d78b4155ddf342ac
Joyal <joyaljoykarunagappally@gmail.com>
2024-07-03 20:19:08 +0530

    Added set

---
+++ 19Sets.py
@@ -0,0 +1,17 @@
+# Set = collection which is unordered, unindexed. No duplicates
+
+
+animals = {"Cat", "Dog", "Cow", "Hen"}
+birds = {"parrot", "crow", "Pigeon", "Hen"}
+
+# animals.add("rabbit")         #add an item
+# animals.remove("Dog")         #remove an item
+# animals.clear()               #clear the whole set
+# animals.update(birds)         #add all elements in the second to first set
+# zoo = animals.union(birds)    #join two set
+
+# print(animals.difference(birds))        #animals - birds
+# print(animals.intersection(birds))          #intersect two sets
+
+# for i in zoo:
+#     print(i)
